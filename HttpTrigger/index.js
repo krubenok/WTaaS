21d03cc314d6fb970177196eb437415ddc683b65
Kyle Rubenok <kyle@rubenok.ca>
2019-10-24 14:42:22 -0400

    Add readme and update package

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -1,11 +1,12 @@
 const request = require("request");
 
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+module.exports = async function(context, req) {
+    context.log("JavaScript HTTP trigger function processed a request.");
 
     if (req.query.name || (req.body && req.body.name)) {
         request(
-            ("https://wunameaas.herokuapp.com/wuami/"+(req.query.name || req.body.name)),
+            "https://wunameaas.herokuapp.com/wuami/" +
+                (req.query.name || req.body.name),
             { json: true },
             (err, res, body) => {
                 if (err) {
@@ -15,16 +16,16 @@ module.exports = async function (context, req) {
                 wtname = body.message;
             }
         );
-        
+
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (wtname)
+            body: "Hello " + wtname
         };
-    }
-    else {
+    } else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body:
+                "Please pass a name on the query string or in the request body"
         };
     }
 };
---
+++ README.md
@@ -0,0 +1,5 @@
+# WTaaS Serverless Demo
+Created for McGill's ECSE437 as a serverless function demo. This relay's [WuTang as a Service](https://wunameaas.herokuapp.com/)'s WuTang name generator to an Azure Functions serverless application. 
+
+## Tech Specs
+Built using the HTTP Trigger Node.JS starter. 
